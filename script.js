2aca0380e57ddfc7ea57a18e6794d4f14750f3c0
trabram <77090732+trabram@users.noreply.github.com>
2023-01-29 17:25:16 +0700

    Add operator and second number input function

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function modulo(a,b)   { return a % b }
 function operate(operator, a, b) { return operator(a, b) }
 
 // elements
-const elemDisplayTop = document.querySelector('.display__top')
-const elemBtnNumbers = document.querySelectorAll('.btn__num');
+const elemDisplayTop   = document.querySelector('.display__top')
+const elemBtnNumbers   = document.querySelectorAll('.btn__num');
+const elemBtnOperators = document.querySelectorAll('.btn__opr');
 
 // variables
 const query = {
@@ -35,13 +36,37 @@ function setNumber(number) {
   if (query.operator) query.num2 = value;
   else                query.num1 = value;
 }
+function setOperator(opr) {
+  switch (opr) {
+    case 'add':
+      query.operator = '+';
+      break;
+    case 'subtract':
+      query.operator = '-';
+      break;
+    case 'multiply':
+      query.operator = '*';
+      break;
+    case 'divide':
+      query.operator = '/';
+      break;
+  }
+}
 
+// 
 elemBtnNumbers.forEach(element => {
   const number = element.dataset.num;
   
   element.onclick = () => {
     setNumber(number);
-    elemDisplayTop.textContent = query.num1;
+    elemDisplayTop.textContent = `${query.num1} ${query.operator} ${query.num2}`;
   };
-
+});
+elemBtnOperators.forEach(element => {
+  const operation = element.dataset.opr;
+  
+  element.onclick = () => {
+    setOperator(element.dataset.opr);
+    elemDisplayTop.textContent = `${query.num1} ${query.operator} ${query.num2}`;
+  };
 });
